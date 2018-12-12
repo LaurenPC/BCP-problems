e9bdaca32ee1352c967e6caa2a03f758e08c7420
Lauren Pariente-Cohen <laurenstavrach@gmail.com>
2018-12-12 14:46:08 -0500

    BCP workshop projects

---
+++ Pre-Work Expressions, Variables, and TDD/index.js
@@ -0,0 +1,31 @@
+
+
+//  *= 200 /= 4);
+
+// // YOUR CODE BELOW
+
+
+// describe("happyNum", function() {
+
+//   it("should be equal to 5", function() {
+//     expect(happyNum).toEqual(5);
+//   });
+
+// });
+
+// Take the Shortcut
+// Given a starting value assigned to the happyNum variable, use at least three of the following operators to reassign the value in happyNum until it is equal to 5:
+
+// +=
+// -=
+// *=
+// /=
+// ++
+
+let happyNum = 1000;
+
+happyNum /= 2;
+
+happyNum /= 50;
+
+happyNum -= 5; 
